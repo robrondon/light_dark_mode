76b6cc3012a2de9d0c1d6de5ea170bf879cfb2ff
Robert Rondon <robrondon11@gmail.com>
2022-02-10 16:11:55 -0300

    adding local storage to track theme

--- script.js
+++ script.js
@@ -36,12 +36,24 @@ const switchTheme = function (e) {
   const checked = e.target.checked;
   if (checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
     darkMode();
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
     lightMode();
   }
 };
 
 // Event Listeners
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage for Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
